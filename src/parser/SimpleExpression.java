53db7511253da194050dcdab1e1a8feb2091ce15
Oscar Luis Sanchez <tinocoscar11@unitec.edu>
2023-03-26 21:16:56 -0600

    Simple_expression case

--- src/parser/SimpleExpression.java
+++ src/parser/SimpleExpression.java
@@ -1,15 +1,18 @@
 package parser;
 
+import java.util.ArrayList;
+
 public class SimpleExpression {
     private Term firstTerm;
     private Term optionalTerm;
-
+    private ArrayList<AddOp> addOps;
     public SimpleExpression(Term firstTerm) {
         this.firstTerm = firstTerm;
     }
 
-    public SimpleExpression(Term firstTerm, Term optionalTerm) {
+    public SimpleExpression(Term firstTerm,ArrayList<AddOp> addOps, Term optionalTerm) {
         this.firstTerm = firstTerm;
+        this.addOps = addOps;
         this.optionalTerm = optionalTerm;
     }
 }
